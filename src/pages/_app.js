7def5464cf564301de881cdde557c86c22c002c9
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2021-11-21 21:36:16 +0100

    feat(front): Add ProtectedRoutes logic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
+import ProtectedRoutes from 'src/helper/ProtectedRoutes/ProtectedRoutes';
 import AppProviders from 'src/providers/AppProviders';
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps, router }) => {
 	return (
 		<AppProviders>
-			<Component {...pageProps} />
+			<ProtectedRoutes router={router}>
+				<Component {...pageProps} />
+			</ProtectedRoutes>
 		</AppProviders>
 	);
-}
+};
 
 export default MyApp;
