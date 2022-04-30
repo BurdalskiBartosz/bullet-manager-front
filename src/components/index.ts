93b397bd701626bce1d3904985f5f55cdbd27353
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-04-30 05:37:58 +0200

    Add Icon and IconButton components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 // atoms
+export { default as Icon } from './atoms/Icon';
 export { default as Input } from './atoms/Input';
 export { default as Font } from './atoms/Font';
 export { default as Button } from './atoms/Button';
+export { default as IconButton } from './atoms/IconButton';
 export { default as Link } from './atoms/Link';
 // molecules
 
