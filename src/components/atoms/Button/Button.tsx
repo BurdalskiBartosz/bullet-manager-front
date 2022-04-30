056f57c89c6a411b545be739176ebf0c55ee1340
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-04-30 05:38:18 +0200

    Start working on Button component

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,10 +1,17 @@
 import { FC, ReactChild } from 'react';
+import { StyledButton } from './Button.styles';
 
 type Props = {
 	children: ReactChild;
+	fn?: Function;
+	type?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
 };
 
-const Button: FC<Props> = ({ children }) => {
-	return <button>{children}</button>;
+const Button: FC<Props> = ({ children, fn = () => {}, type = 'button' }) => {
+	return (
+		<StyledButton type={type} onClick={() => fn()}>
+			{children}
+		</StyledButton>
+	);
 };
 export default Button;
