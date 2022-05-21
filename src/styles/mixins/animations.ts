d649a0a27083ef6a89bc9882218afb7598ce8c57
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-05-21 18:13:28 +0200

    Add animations to theme

---
+++ src/styles/mixins/animations.ts
@@ -0,0 +1,25 @@
+export const animations = {
+	menuTransition: (color: string, secondColor: string) => `
+		position: relative;
+		color: ${color};
+		transition: 0.2s linear;
+		&::before {
+			content: '';
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			background-color: ${color};
+			z-index: -1;
+			opacity: 0;
+			transition: 0.3s linear;
+		}
+		&:hover {
+			color: ${secondColor};
+			&::before {
+				opacity: .7;
+			}
+		}
+	`
+};
