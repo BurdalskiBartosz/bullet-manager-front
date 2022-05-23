4f675d095ddf6115d8817ef847188fb21c7ea45c
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-05-23 03:29:08 +0200

    Add menu translations

--- src/i18n/pl/index.tsx
+++ src/i18n/pl/index.tsx
@@ -2,10 +2,12 @@ import login from './login.json';
 import registration from './registration.json';
 import forms from './forms.json';
 import errors from './errors.json';
+import menu from './menu.json';
 
 export const plTranslations = {
 	...login,
 	...registration,
 	...forms,
-	...errors
+	...errors,
+	...menu
 };
