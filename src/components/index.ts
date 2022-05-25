33ed49fd71be6a2a584d3a6a26bedf9aca1bba75
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-05-25 04:01:10 +0200

    Update imports and change import file component from tsx to ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,6 +7,7 @@ export { default as IconButton } from './atoms/IconButton';
 export { default as Link } from './atoms/Link';
 // molecules
 export { default as Breadcrumbs } from './molecules/Breadcrumbs';
+export { default as SearchInput } from './molecules/SearchInput';
 // organism
 export { default as HomeMenu } from './organisms/menus/HomeMenu';
 export { default as SideMenu } from './organisms/menus/SideMenu';
