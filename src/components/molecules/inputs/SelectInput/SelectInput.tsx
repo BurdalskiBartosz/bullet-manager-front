e130c343209b79db9a295eef417431e83df1dac3
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-07-02 16:51:03 +0200

    Install and implement react-select library

--- src/components/molecules/inputs/SelectInput/SelectInput.tsx
+++ src/components/molecules/inputs/SelectInput/SelectInput.tsx
@@ -1,23 +1,82 @@
+import Font from 'components/atoms/Font';
 import { FC } from 'react';
+import { Controller } from 'react-hook-form';
+import Select from 'react-select';
 
 type Props = {
+	id: string;
+	label: string;
 	getOptionsFn: Function;
 	keyValue: string;
+	control: any;
+	multi?: boolean;
 };
-
-const SelectInput: FC<Props> = ({ getOptionsFn, keyValue }) => {
+const customStyles = {
+	option: (provided: any, state: any) => ({}),
+	control: (provided: any) => ({
+		...provided,
+		border: '1px solid #B7B7B7',
+		borderRadius: '15px',
+		paddingLeft: '5px'
+	})
+};
+const SelectInput: FC<Props> = ({
+	id,
+	label,
+	control,
+	getOptionsFn,
+	keyValue,
+	multi = false
+}) => {
 	const { data } = getOptionsFn();
+
+	const selectOptions = data.map((option: any) => {
+		return {
+			label: option[keyValue],
+			value: option.id
+		};
+	});
+
 	return (
-		<select>
-			{data &&
-				data.map((option: any) => {
+		<div>
+			<Font htmlFor={id} variant="label">
+				{label}
+			</Font>
+			<Controller
+				name={id}
+				control={control}
+				render={({ field }) => {
+					const { onChange, value, onBlur } = field;
+					console.log(value);
 					return (
-						<option value={option.id} key={option.id}>
-							{option[keyValue]}
-						</option>
+						<Select
+							styles={customStyles}
+							options={selectOptions}
+							isMulti={multi}
+							onBlur={onBlur}
+							onChange={(data) =>
+								onChange(
+									multi
+										? data?.map(
+												(option: any) => option.value
+										  )
+										: data.value
+								)
+							}
+							value={
+								multi
+									? selectOptions.filter((option: any) =>
+											value?.includes(option.value)
+									  )
+									: selectOptions.find(
+											(c: any) => c.value === value
+									  )
+							}
+						/>
 					);
-				})}
-		</select>
+				}}
+			/>
+		</div>
 	);
 };
 
