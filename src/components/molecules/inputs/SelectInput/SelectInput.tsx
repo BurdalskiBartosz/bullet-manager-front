b00322edb8096b967a598b491334e7b2ad392c6b
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-07-05 23:43:31 +0200

    Add some styles to SelectInput

--- src/components/molecules/inputs/SelectInput/SelectInput.tsx
+++ src/components/molecules/inputs/SelectInput/SelectInput.tsx
@@ -12,12 +12,38 @@ type Props = {
 	multi?: boolean;
 };
 const customStyles = {
-	option: (provided: any, state: any) => ({}),
+	option: (provided: any, state: any) => ({
+		...provided,
+		fontSize: '14px',
+		padding: '5px 0 5px 20px',
+		cursor: 'pointer',
+		fontWeight: 'bold'
+	}),
 	control: (provided: any) => ({
 		...provided,
 		border: '1px solid #B7B7B7',
 		borderRadius: '15px',
 		paddingLeft: '5px'
+	}),
+	singleValue: (provided: any) => ({
+		...provided,
+		fontSize: '13px'
+	}),
+	menuList: (base: any) => ({
+		...base,
+		'::-webkit-scrollbar': {
+			width: '4px',
+			height: '0px'
+		},
+		'::-webkit-scrollbar-track': {
+			background: '#f1f1f1'
+		},
+		'::-webkit-scrollbar-thumb': {
+			background: '#888'
+		},
+		'::-webkit-scrollbar-thumb:hover': {
+			background: '#555'
+		}
 	})
 };
 const SelectInput: FC<Props> = ({
