ddba603e9b1c8814e5449ad3b05e04f3836098e4
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-07-07 03:15:53 +0200

    Add finished SelectInput

--- src/components/molecules/inputs/SelectInput/SelectInput.tsx
+++ src/components/molecules/inputs/SelectInput/SelectInput.tsx
@@ -1,57 +1,38 @@
 import Font from 'components/atoms/Font';
 import { FC } from 'react';
 import { Controller } from 'react-hook-form';
-import Select from 'react-select';
+import Select, { GroupBase } from 'react-select';
+import { customStyles, StyledInputWrapper } from './SelectInput.style';
+declare module 'react-select/dist/declarations/src/Select' {
+	export interface Props<
+		Option,
+		IsMulti extends boolean,
+		Group extends GroupBase<Option>
+	> {
+		isError: boolean;
+	}
+}
 
-type Props = {
+type tProps = {
 	id: string;
 	label: string;
 	getOptionsFn: Function;
 	keyValue: string;
 	control: any;
 	multi?: boolean;
+	error: {
+		isError: boolean;
+		errorMessage: string;
+	};
 };
-const customStyles = {
-	option: (provided: any, state: any) => ({
-		...provided,
-		fontSize: '14px',
-		padding: '5px 0 5px 20px',
-		cursor: 'pointer',
-		fontWeight: 'bold'
-	}),
-	control: (provided: any) => ({
-		...provided,
-		border: '1px solid #B7B7B7',
-		borderRadius: '15px',
-		paddingLeft: '5px'
-	}),
-	singleValue: (provided: any) => ({
-		...provided,
-		fontSize: '13px'
-	}),
-	menuList: (base: any) => ({
-		...base,
-		'::-webkit-scrollbar': {
-			width: '4px',
-			height: '0px'
-		},
-		'::-webkit-scrollbar-track': {
-			background: '#f1f1f1'
-		},
-		'::-webkit-scrollbar-thumb': {
-			background: '#888'
-		},
-		'::-webkit-scrollbar-thumb:hover': {
-			background: '#555'
-		}
-	})
-};
-const SelectInput: FC<Props> = ({
+
+const SelectInput: FC<tProps> = ({
 	id,
 	label,
 	control,
 	getOptionsFn,
 	keyValue,
+	error,
 	multi = false
 }) => {
 	const { data } = getOptionsFn();
@@ -64,7 +45,7 @@ const SelectInput: FC<Props> = ({
 	});
 
 	return (
-		<div>
+		<StyledInputWrapper isError={error.isError}>
 			<Font htmlFor={id} variant="label">
 				{label}
 			</Font>
@@ -73,10 +54,10 @@ const SelectInput: FC<Props> = ({
 				control={control}
 				render={({ field }) => {
 					const { onChange, value, onBlur } = field;
-					console.log(value);
 					return (
 						<Select
 							styles={customStyles}
+							isError={error.isError}
 							options={selectOptions}
 							isMulti={multi}
 							onBlur={onBlur}
@@ -102,7 +83,13 @@ const SelectInput: FC<Props> = ({
 					);
 				}}
 			/>
-		</div>
+			<Font
+				variant="inputError"
+				style={{ alignSelf: 'flex-end', height: '10px' }}
+			>
+				{error.isError ? error.errorMessage : ''}
+			</Font>
+		</StyledInputWrapper>
 	);
 };
 
