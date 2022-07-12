64f040bbf14d3453bee1d1b8e570a572152ee4dc
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-07-12 20:12:31 +0200

    Avoid to close all modals when click outside if there are more than one modal

--- src/components/molecules/inputs/CalendarInput/CalendarInput.tsx
+++ src/components/molecules/inputs/CalendarInput/CalendarInput.tsx
@@ -3,34 +3,63 @@ import Calendar from 'react-calendar';
 import { Controller } from 'react-hook-form';
 
 import Font from 'components/atoms/Font';
-import { FC } from 'react';
+import { FC, Ref } from 'react';
 import { StyledWrapper } from './CalendarInput.style';
+import Modal from 'components/molecules/Modal';
+import useModal from 'hooks/useModal';
 
 type tProps = {
 	id: string;
 	label: string;
 	control: any;
+	refParent?: {
+		current: HTMLElement;
+	};
 	error: {
 		isError: boolean;
 		errorMessage: string;
 	};
 };
 
-const CalendarInput: FC<tProps> = ({ id, label, control, error }) => {
+const CalendarInput: FC<tProps> = ({
+	id,
+	label,
+	control,
+	error,
+	refParent
+}) => {
+	const { handleCloseModal, handleOpenModal, isOpen } = useModal();
+
 	return (
 		<StyledWrapper isError={error.isError}>
 			<Font htmlFor={id} variant="label">
 				{label}
 			</Font>
 			<div>
-				<Controller
-					name={id}
-					control={control}
-					render={({ field }) => {
-						const { onChange, value } = field;
-						return <Calendar onChange={onChange} value={value} />;
-					}}
-				/>
+				<button type="button" onClick={handleOpenModal}>
+					OPEN
+				</button>
+				{isOpen ? (
+					<Modal
+						header="Dodaj zadanie"
+						handleClose={handleCloseModal}
+						customParent={refParent?.current}
+					>
+						<Controller
+							name={id}
+							control={control}
+							render={({ field }) => {
+								const { onChange, value } = field;
+								return (
+									<Calendar
+										onChange={onChange}
+										value={value}
+									/>
+								);
+							}}
+						/>
+					</Modal>
+				) : null}
 			</div>
 			<Font
 				variant="inputError"
