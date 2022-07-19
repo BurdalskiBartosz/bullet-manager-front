55faf475e6023af138723c5e293ee7868d325e7a
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-07-19 03:13:57 +0200

    Create InputBase component

---
+++ src/components/atoms/InputBase/InputBase.tsx
@@ -0,0 +1,71 @@
+import { FC, ReactNode } from 'react';
+import { useTranslation } from 'react-i18next';
+import Font from '../Font';
+import { tIconNames } from '../Icon/Icon';
+import {
+	StyledIconButton,
+	StyledInnerWrapper,
+	StyledInput,
+	StyledWrapper
+} from './InputBase.style';
+
+export type tInputBase = {
+	id: string;
+	label: string;
+	type?: string;
+	register?: Function;
+	fullWidth?: boolean;
+	isDisabled?: boolean;
+	children?: ReactNode | ReactNode[];
+	icon?: {
+		iconName: tIconNames;
+		fn: () => void;
+	};
+	error: {
+		isError: boolean;
+		errorMessage: string;
+	};
+};
+
+const InputBase: FC<tInputBase> = ({
+	id,
+	label,
+	type = 'text',
+	register,
+	error,
+	isDisabled,
+	icon,
+	children,
+	fullWidth = true
+}) => {
+	const { t } = useTranslation();
+
+	return (
+		<StyledWrapper isError={error.isError}>
+			<Font htmlFor={id} variant="label">
+				{label}
+			</Font>
+			<StyledInnerWrapper>
+				<StyledInput
+					id={id}
+					type={type}
+					disabled={isDisabled}
+					placeholder={t(`${label} placeholder`)}
+					fullWidth={fullWidth}
+					{...(register ? register(id) : {})}
+				/>
+				{icon && (
+					<StyledIconButton iconName={icon.iconName} fn={icon.fn} />
+				)}
+			</StyledInnerWrapper>
+			<Font
+				variant="inputError"
+				style={{ alignSelf: 'flex-end', height: '10px' }}
+			>
+				{error.isError ? error.errorMessage : ''}
+			</Font>
+		</StyledWrapper>
+	);
+};
+
+export default InputBase;
