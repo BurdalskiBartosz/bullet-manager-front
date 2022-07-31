125f18fb942e92758cbfb9c945a132ab6263d004
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-07-31 23:41:24 +0200

    Delete unused test

--- src/components/atoms/Button/Button.test.tsx
+++ src/components/atoms/Button/Button.test.tsx
@@ -1,19 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import Button from './Button';
-import userEvent from '@testing-library/user-event';
 
 describe('renders learn react link', () => {
 	it('should', () => {
 		render(<Button>Test buttona</Button>);
 		const linkElement = screen.getByText(/Test buttona/i);
 		expect(linkElement).toBeInTheDocument();
-	});
-
-	test('clear', async () => {
-		render(<textarea defaultValue="Hello, World!" />);
-
-		await userEvent.clear(screen.getByRole('textbox'));
-
-		expect(screen.getByRole('textbox')).toHaveValue('');
 	});
 });
