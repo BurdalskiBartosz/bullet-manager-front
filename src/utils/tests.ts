b5d5798512ea6b0f456f1f8ba1df5243eb125f59
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-08-09 22:42:53 +0200

    Update tests utility

--- src/utils/tests.ts
+++ src/utils/tests.ts
@@ -1,7 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, RenderOptions } from '@testing-library/react';
+import { ReactElement } from 'react';
 import Providers from '../providers';
 
-const customRender = (ui: any) => render(ui, { wrapper: Providers });
+const customRender = (
+	ui: ReactElement,
+	options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: Providers, ...options });
 
 export * from '@testing-library/react';
 
