eff20696e9ce42e4a2f0984a1e9cf073baa39a58
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-08-21 00:46:47 +0200

    Update redux store and add redux hooks

--- src/store/api.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import { tasksApi } from './api/task';
-
-export const rootReducer = combineReducers({
-	[tasksApi.reducerPath]: tasksApi.reducer
-});
-export type tRootState = ReturnType<typeof rootReducer>;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { rootReducer } from './api';
+import {
+	combineReducers,
+	configureStore,
+	PreloadedState
+} from '@reduxjs/toolkit';
 import { tasksApi } from './api/task';
 
 const middlewares = [tasksApi.middleware];
 
-const store = configureStore({
-	reducer: rootReducer,
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(middlewares)
+export const rootReducer = combineReducers({
+	[tasksApi.reducerPath]: tasksApi.reducer
 });
 
-export default store;
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+	return configureStore({
+		reducer: rootReducer,
+		preloadedState,
+		middleware: (getDefaultMiddleware) =>
+			getDefaultMiddleware().concat(middlewares)
+	});
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export type AppStore = ReturnType<typeof setupStore>;
+
+export type AppDispatch = AppStore['dispatch'];
