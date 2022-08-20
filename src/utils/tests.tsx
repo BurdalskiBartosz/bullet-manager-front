1c6bb76d61192c08bb2bbea59f5d0445d05f47e0
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-08-21 01:31:23 +0200

    Update tests utility - import redux store

--- src/utils/tests.tsx
+++ src/utils/tests.tsx
@@ -1,37 +1,27 @@
 import { render, RenderOptions } from '@testing-library/react';
 import { PropsWithChildren } from 'react';
 import { Provider } from 'react-redux';
-import { tasksApi } from 'store/api/task';
-import { configureStore, PreloadedState } from '@reduxjs/toolkit';
-import { AppStore, RootState } from 'store/store';
+import { PreloadedState } from '@reduxjs/toolkit';
+import { AppStore, RootState, setupStore } from 'store/store';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styles/theme';
 import { BrowserRouter } from 'react-router-dom';
-
-const middlewares = [tasksApi.middleware];
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
 	preloadedState?: PreloadedState<RootState>;
 	store?: AppStore;
 }
-
-export function renderWithProviders(
+const reduxStore = setupStore();
+export const renderWithProviders = (
 	ui: React.ReactElement,
 	{
 		preloadedState = {},
 		// Automatically create a store instance if no store was passed in
-		store = configureStore({
-			reducer: {
-				[tasksApi.reducerPath]: tasksApi.reducer
-			},
-			preloadedState,
-			middleware: (getDefaultMiddleware) =>
-				getDefaultMiddleware().concat(middlewares)
-		}),
+		store = reduxStore,
 		...renderOptions
 	}: ExtendedRenderOptions = {}
-) {
-	function Wrapper({ children }: PropsWithChildren<{}>) {
+) => {
+	const Wrapper = ({ children }: PropsWithChildren<{}>) => {
 		return (
 			<BrowserRouter>
 				<Provider store={store}>
@@ -39,11 +29,11 @@ export function renderWithProviders(
 				</Provider>
 			</BrowserRouter>
 		);
-	}
+	};
 
 	// Return an object with the store and all of RTL's query functions
 	return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
-}
+};
 
 export { renderWithProviders as render };
 export * from '@testing-library/react';
