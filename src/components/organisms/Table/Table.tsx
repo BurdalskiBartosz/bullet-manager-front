55912f53f2ba3c34d67e6a3d9ac6629e9287a077
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-10-30 22:14:28 +0100

    Add basic for Table component

--- src/components/organisms/Table/Table.tsx
+++ src/components/organisms/Table/Table.tsx
@@ -1,10 +1,55 @@
-import { Font } from 'components';
+import Font from 'components/atoms/Font';
 import { FC } from 'react';
+import { tTableData } from 'types/table';
+import {
+	StyledTable,
+	StyledTD,
+	StyledTH,
+	StyledTHead,
+	StyledTR,
+	Wrapper
+} from './Table.style';
 
-type tProps = {};
+type tProps = {
+	headData: string[];
+	data: tTableData[];
+};
 
-const Table: FC<tProps> = () => {
-	return <Font style={{ color: 'white' }}>Table</Font>;
+const Table: FC<tProps> = ({ headData, data }) => {
+	return (
+		<Wrapper>
+			<StyledTable>
+				<StyledTHead>
+					<StyledTR>
+						{headData.map((el) => {
+							return (
+								<StyledTH key={el}>
+									<Font>{el}</Font>
+								</StyledTH>
+							);
+						})}
+					</StyledTR>
+				</StyledTHead>
+				<tbody>
+					{data.map(({ id, data }) => {
+						return (
+							<tr key={id}>
+								{data.map(({ key, value }) => {
+									if (typeof value === 'object')
+										return <StyledTD>{value}</StyledTD>;
+									return (
+										<StyledTD key={key}>
+											<Font>{value}</Font>
+										</StyledTD>
+									);
+								})}
+							</tr>
+						);
+					})}
+				</tbody>
+			</StyledTable>
+		</Wrapper>
+	);
 };
 
 export default Table;
---
+++ src/utils/table.ts
@@ -0,0 +1,14 @@
+import { tTableData } from 'types/table';
+import { UserTask } from 'view/Tasks/UserTasks';
+
+type tData = UserTask;
+
+export const transformApiToTableData = (data: tData[], offset: number) =>
+	data.map(({ id, ...rest }, index) => ({
+		id: id ?? '',
+		data: [
+			...(Object.keys(rest) as Array<keyof typeof rest>).map((key) => {
+				return { key, value: rest[key] };
+			})
+		]
+	})) as tTableData[];
